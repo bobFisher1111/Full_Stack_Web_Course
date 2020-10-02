f9fffb07eedebf47e47828caf0e3d86cc3a6f1f2
bobFisher1111 <rroseberry@twu.edu>
2020-10-01 18:38:14 -0700

    Update

--- 10_APIs/Sign_Up_Page/Newsletter-Signup/app.js
+++ 10_APIs/Sign_Up_Page/Newsletter-Signup/app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const https = require("https");
 
 // create app constant new instance of express
 const app = express();
@@ -19,13 +20,48 @@ app.get("/", function(req, res){
 // post the data from the sign up and console log it
 app.post("/", function(req, res){
     // saving data as a veriable thats being typed in
-    var firstName = req.body.fName;
-    var lastName = req.body.lName;
-    var emailName = req.body.email;
+    const firstName = req.body.fName;
+    const lastName = req.body.lName;
+    const email = req.body.email;
 
-    console.log(firstName, lastName, emailName);
+    //console.log(firstName, lastName, emailName);
+    // going to be using mailchimp api
+    const data = {
+        members: [
+            {
+                email_address: email,
+                status: "subscribed",
+                merge_fields: {
+                    FNAME: firstName,
+                    LNAME: lastName
+                }
+            }
+        ]
+    };
+
+    // turning data into json file
+    const jsonData = JSON.stringify(data);
+    const url = "https://us2.api.mailchimp.com/3.0/list/f019f74e3b";
+    const options = {
+        method: "POST",
+        auth: "turtle1:d02a8fdb3c6acd3c45fa2ef3b26a330f-us2"
+    }
+    // make post request:
+    const request = https.request(url, options, function(response){
+        response.on("data", function(data){
+            console.log(JSON.parse(data));
+        })
+    })
+    request.write(jsonData);
+    request.end();
+
 })
 // setup running on port 3000
 app.listen(3000, function(){
     console.log("Server is running on port 3000");
 });
+
+// API Key: https://us2.admin.mailchimp.com/account/api/
+// d02a8fdb3c6acd3c45fa2ef3b26a330f-us2
+// unique id: list id
+// f019f74e3b

27e7accd2030294bccaf5180507e60a963197ac8
bobFisher1111 <rroseberry@twu.edu>
2020-10-25 20:31:29 -0700

    Update

---
+++ 15_Mongoose/FruitsProject/app.js
@@ -0,0 +1,6 @@
+// accuire mongoose
+const mongoose = require('mongoose');
+
+// connect to MongoDB
+mongoose.connect("mongodb://localhost:27017/fruitsDB"); // at the end give the name of the database
+

9f079da0e300fecaa63e7b99e982954dafa9db3b
bobFisher1111 <rroseberry@twu.edu>
2020-11-05 20:11:48 -0800

    Update

---
+++ 15_Mongoose/mongodb_atlas.ts
@@ -0,0 +1,21 @@
+/*
+    - Open the shell and connect to mongodb atlas:
+        - mongo "mongodb+srv://cluster0.agqwx.mongodb.net/Cluster0" --username userName //in the book
+        - password: in book
+    - connect application:
+        - add this in the application where we had 
+        - mongodb+srv://userName:Test123@cluster0.agqwx.mongodb.net/test?retryWrites=true&w=majority
+
+    - Connect Application to https://www.heroku.com/:
+        - node.js setup:
+            https://devcenter.heroku.com/articles/getting-started-with-nodejs#set-up
+        - Site is at:
+            https://ancient-woodland-72098.herokuapp.com/
+    - login and push code to server:
+        - heroku login
+        - git init
+        - git add .
+        - git commit -m "message"
+        - heroku create
+        - git push heroku master
+*/

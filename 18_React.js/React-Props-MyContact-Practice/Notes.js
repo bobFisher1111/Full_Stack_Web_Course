8e45e4b1ec90f2590d3ef32695d66415504ccb54
bobFisher1111 <rroseberry@twu.edu>
2020-11-29 18:56:46 -0800

    Update

---
+++ 18_React.js/React-Props-MyContact-Practice/Notes.js
@@ -0,0 +1,5 @@
+/*
+    - Notes: Mapping Data Components
+        - map function:
+            - creates a new array with the results of calling a function for every array element.
+*/
--- 18_React.js/React-Props-MyContact-Practice/src/components/App.jsx
+++ 18_React.js/React-Props-MyContact-Practice/src/components/App.jsx
@@ -3,6 +3,20 @@ import Card from "./Card";
 import contacts from "../contacts";
 import Avatar from "./Avatar";
 
+// function will be passed into the map() method
+function createCard(contact) {
+  // must include an unique ID for the mapping called key, we already have value called id from contacts
+  return <Card 
+    key={contact.id} // this will remove a warning
+    name={contact.name}
+    img={contact.imgURL}
+    tel={contact.phone}
+    email={contact.email}
+    id={contact.id}
+  />
+}
+
+// Using the map function to loop through Card, passing in function
 function App() {
   return (
     <div>
@@ -10,26 +24,31 @@ function App() {
       <Avatar
         img="https://i.kinja-img.com/gawker-media/image/upload/s--AwaSX1yO--/c_fill,fl_progressive,g_center,h_900,q_80,w_1600/198kxsa1st1aojpg.jpg"
       />
-      <Card
-        name={contacts[0].name}
-        img={contacts[0].imgURL}
-        tel={contacts[0].phone}
-        email={contacts[0].email}
-      />
-      <Card
-        name={contacts[1].name}
-        img={contacts[1].imgURL}
-        tel={contacts[1].phone}
-        email={contacts[1].email}
-      />
-      <Card
-        name={contacts[2].name}
-        img={contacts[2].imgURL}
-        tel={contacts[2].phone}
-        email={contacts[2].email}
-      />
+      {contacts.map(createCard)}
     </div>
   );
 }
+
+// old way of doing, now using map method: in the div above
+/*
+  <Card
+    name={contacts[0].name}
+    img={contacts[0].imgURL}
+    tel={contacts[0].phone}
+    email={contacts[0].email}
+  />
+  <Card
+    name={contacts[1].name}
+    img={contacts[1].imgURL}
+    tel={contacts[1].phone}
+    email={contacts[1].email}
+  />
+  <Card
+    name={contacts[2].name}
+    img={contacts[2].imgURL}
+    tel={contacts[2].phone}
+    email={contacts[2].email}
+  />
+*/
 
 export default App;

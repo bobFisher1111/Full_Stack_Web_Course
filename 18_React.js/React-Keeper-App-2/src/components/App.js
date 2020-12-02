1e7f83ecf5935da1c62137f0aa46f23f535c27a6
bobFisher1111 <rroseberry@twu.edu>
2020-12-01 19:45:43 -0800

    Update Keeper App 2 include arrow function & map

---
+++ 18_React.js/React-Keeper-App-2/src/components/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+
+// added anonymous function as arrow function
+function App() {
+  return (
+    <div>
+      <Header />
+        {notes.map(x =>(
+          <Note
+            key={x.key}
+            title={x.title}
+            content={x.content}
+          />
+        ))}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
